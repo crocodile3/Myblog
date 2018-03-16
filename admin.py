eb43b40618004fec874455034865c5366f24038c
crocodile3 <33323847+crocodile3@users.noreply.github.com>
2018-03-16 18:11:24 +0800

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from blog.models import *
-
-
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ['id','title','category','date_time','update_time']
-# Register your models here.
-admin.site.register(Blog,BlogAdmin)
